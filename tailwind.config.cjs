271098a6cc0bded8afcc66c7e7b88a69e1eee2e4
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-01-07 14:21:01 +0800

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("daisyui")
+	],
+	darkMode: ['class', '[data-theme="dark"]'],
+	daisyui: {
+		themes: [
+            "light",
+            "dark"
+        ]
+	}
+}
