fb09c183efc36d6927b8e6ebdadfeaf7b7854352
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-01-23 16:37:57 +0800

    feat: add identicons

---
+++ src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import { useStore } from '@nanostores/react';
+
+import { account } from "../scripts/stores";
+
+export default function Menu() {
+    const $account = useStore(account);
+
+    if ($account === "") {
+        return (
+            <>
+                <li>
+                    <a href="/signin/" className="font-semibold no-underline">
+                        Signin
+                    </a>
+                </li>
+                <li>
+                    <a href="/login/" className="font-semibold no-underline">
+                        Login
+                    </a>
+                </li>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <li>
+                    <a href="/signout/" className="font-semibold no-underline">
+                        Signout
+                    </a>
+                </li>
+            </>
+        )
+    }
+}
