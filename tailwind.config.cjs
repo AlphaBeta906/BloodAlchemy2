6256981d8e96afdf464cac384999abe22e35d99b
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-01-24 08:50:57 +0800

    fix: remove unessesary code nfrom addelems.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	mode: "jit",
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [
 		require("daisyui")
 	],
-	darkMode: ['class', '[data-theme="dark"]'],
+	darkMode: ["class", "[data-theme=\"dark\"]"],
 	daisyui: {
 		themes: [
 			"light",
 			"dark"
 		]
 	}
-}
+};
