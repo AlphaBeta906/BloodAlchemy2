eaae77a5d12b82857af545ce8d9b924822c2a0fe
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-01-30 08:34:31 +0800

    feat: ue react query

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,67 +1,66 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import PropTypes from "prop-types";
 
 import Error from "./Error";
 import ElemBox from "./ElementBox";
 import Avatar from "./Avatar";
+import QueryWrapper from "./QueryWrapper";
+
+function Body({ name }) {
+	const { isLoading, error, data } = useQuery({
+		queryKey: ["element"],
+		queryFn: async () => {
+			const result = await fetch(`/api/element?name=${name}`);
+
+			if (result.status === 204) {
+				return 204;
+			}
+
+			const rjson = result.json();
+
+			return rjson;
+		}
+	});
+
+	if (isLoading) return "Loading...";
+
+	if (error) return "An error has occurred: " + error.message;
+
+	if (data === 204) {
+		return (
+			<Error code="404">
+				This element does not exist.
+			</Error>
+		);
+	}
+
+	return (
+		<>
+			<center className="p-10">
+				<ElemBox body={data} width={100} />
+			</center>
+
+			<div className="mx-10">
+				<div><b>ID:</b> #{data.id}</div>
+				<div className="flex items-center">
+					<b>Creator:</b>&thinsp;&thinsp;<Avatar username={data.creator} small={true} />&thinsp;&thinsp;{data.creator}
+				</div>
+			</div>
+		</>
+	);
+}
 
 export default function Element({ name }) {
-	const [status, setStatus] = useState(null);
-	const [body, setBody] = useState({});
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		console.log("aaaa");
-		getData();
-	}, []);
-
-	const getData = async () => {
-		const result = await fetch(`/api/element?name=${name}`, {
-			method: "GET"
-		});
-
-		setStatus(result.status);
-
-		if (result.status === 200) {
-			const rjson = await result.json();
-
-			setBody(rjson);
-		}
-
-		setIsLoading(false);
-	};
-
-	if (!isLoading) {
-		if (status === 204) {
-			return (
-				<Error code="404">
-					This element does not exist.
-				</Error>
-			);
-		} else if (status === 200) {
-			return (
-				<>
-					<center className="p-10">
-						<ElemBox body={body} width={100} />
-					</center>
-
-					<div className="mx-10">
-						<div><b>ID:</b> #{body.id}</div>
-						<div className="flex items-center">
-							<b>Creator:</b>&thinsp;&thinsp;<Avatar username={body.creator} small={true} />&thinsp;&thinsp;{body.creator}
-						</div>
-					</div>
-				</>
-			);
-		} else {
-			return (
-				<Error code="500">
-					The server died.
-				</Error>
-			);
-		}
-	}
+	return (
+		<QueryWrapper>
+			<Body name={name} />
+		</QueryWrapper>
+	);
 }
+
+Body.propTypes = {
+	name: PropTypes.string.isRequired
+};
 
 Element.propTypes = {
 	name: PropTypes.string.isRequired
